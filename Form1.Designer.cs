1fc2b612fba4d09da345d7bb07e668c1e5428b9f
Leo <47742726+leonid1123@users.noreply.github.com>
2022-02-22 09:09:32 +0300

    double check stage 1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,8 +30,6 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -40,16 +38,21 @@
             this.button3 = new System.Windows.Forms.Button();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
-            this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(408, 250);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(389, 35);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(91, 26);
+            this.button1.Size = new System.Drawing.Size(323, 44);
             this.button1.TabIndex = 1;
             this.button1.Text = "Добавить";
             this.button1.UseVisualStyleBackColor = true;
@@ -57,127 +60,151 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(512, 282);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(539, 85);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(173, 22);
+            this.textBox1.Size = new System.Drawing.Size(173, 26);
             this.textBox1.TabIndex = 2;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(512, 310);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(173, 22);
-            this.textBox2.TabIndex = 3;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(512, 341);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(173, 22);
-            this.textBox3.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(405, 288);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(393, 91);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.Size = new System.Drawing.Size(91, 20);
             this.label1.TabIndex = 5;
             this.label1.Text = "Название";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(405, 316);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(393, 119);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 16);
+            this.label2.Size = new System.Drawing.Size(109, 20);
             this.label2.TabIndex = 6;
             this.label2.Text = "Количество";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(405, 344);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(393, 147);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(40, 16);
+            this.label3.Size = new System.Drawing.Size(107, 20);
             this.label3.TabIndex = 7;
-            this.label3.Text = "Цена";
+            this.label3.Text = "Цена за ед.";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(563, 112);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 473);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(180, 16);
+            this.label4.Size = new System.Drawing.Size(234, 20);
             this.label4.TabIndex = 8;
             this.label4.Text = "Итоговая стоимость: 0руб.";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(408, 91);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(397, 262);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(93, 58);
+            this.button2.Size = new System.Drawing.Size(315, 58);
             this.button2.TabIndex = 9;
             this.button2.Text = "Удалить выбранное";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(408, 172);
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(397, 340);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(93, 55);
+            this.button3.Size = new System.Drawing.Size(313, 55);
             this.button3.TabIndex = 10;
             this.button3.Text = "Изменить количество";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(522, 188);
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(485, 401);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.Size = new System.Drawing.Size(133, 26);
             this.textBox4.TabIndex = 11;
             // 
             // checkedListBox1
             // 
+            this.checkedListBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(12, 86);
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 35);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(371, 378);
+            this.checkedListBox1.Size = new System.Drawing.Size(371, 429);
             this.checkedListBox1.TabIndex = 12;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(78, 34);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(63, 16);
-            this.label5.TabIndex = 13;
-            this.label5.Text = "Корзина";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(405, 372);
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(393, 175);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(104, 16);
+            this.label6.Size = new System.Drawing.Size(133, 20);
             this.label6.TabIndex = 15;
             this.label6.Text = "Ед. измерения";
             // 
-            // textBox5
+            // numericUpDown1
             // 
-            this.textBox5.Location = new System.Drawing.Point(512, 369);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(173, 22);
-            this.textBox5.TabIndex = 14;
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown1.Location = new System.Drawing.Point(539, 113);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(173, 26);
+            this.numericUpDown1.TabIndex = 16;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown2.Location = new System.Drawing.Point(539, 141);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(116, 26);
+            this.numericUpDown2.TabIndex = 17;
+            this.numericUpDown2.ThousandsSeparator = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(661, 147);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 20);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "руб.";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "кг.",
+            "г.",
+            "шт."});
+            this.comboBox1.Location = new System.Drawing.Point(539, 173);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(173, 28);
+            this.comboBox1.TabIndex = 19;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(786, 607);
+            this.ClientSize = new System.Drawing.Size(722, 510);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.label5);
             this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.button3);
@@ -186,13 +213,13 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Список продуктов";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -202,8 +229,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
@@ -212,9 +237,11 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,11 +15,11 @@ namespace ShopList
         class Food
         {
             public string Name { set; get; }
-            public int Price { set; get; }
-            public int Quantity { set; get; }
+            public decimal Price { set; get; }
+            public decimal Quantity { set; get; }
             public string Mesurement { set; get; }
 
-            public Food(string name,int price,int quantity,string mesurenemt)
+            public Food(string name, decimal price, decimal quantity, string mesurenemt)
             {
                 Name = name;
                 Price = price;
@@ -32,37 +32,66 @@ namespace ShopList
         {
             InitializeComponent();
         }
-            
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Food newFood = new Food(textBox1.Text.Trim(), int.Parse(textBox3.Text), int.Parse(textBox2.Text), textBox5.Text);
-            FoodList.Add(newFood);
-            checkedListBox1.Items.Add(newFood.Name + " " + newFood.Price.ToString() + "p." + newFood.Quantity.ToString() +" "+ newFood.Mesurement);
-            int sum=0;
-            foreach (Food food in FoodList)
+            decimal sum = 0;
+            if (textBox1.Text.Trim().Length > 0 && numericUpDown1.Value > 0 && numericUpDown2.Value > 0)
             {
-                sum+=food.Price;
+                bool gotDouble = false;
+                foreach (Food food in FoodList)
+                {
+                    if (food.Name == textBox1.Text.Trim())
+                    {
+                        gotDouble = true;
+                    }
+                    Console.WriteLine(food.Name);
+                    Console.WriteLine(gotDouble);
+                }
+                if (!gotDouble)
+                {
+                    Food newFood = new Food(textBox1.Text.Trim(), numericUpDown2.Value, numericUpDown1.Value, comboBox1.Text);
+                    FoodList.Add(newFood);
+                    checkedListBox1.Items.Add(newFood.Name + " " + newFood.Price.ToString() + "p." + newFood.Quantity.ToString() + " " + newFood.Mesurement);
+
+                }
+                else
+                {
+                    foreach (Food food in FoodList)
+                    {
+                        if (food.Name == textBox1.Text.Trim())
+                        {
+                            food.Quantity += numericUpDown1.Value;
+                        }
+                    }
+                } //TODO написать метод для обновления списка и привязать его ко всем ситуациям
+                foreach (Food food in FoodList)
+                {
+                    sum += food.Price * food.Quantity;
+                }
+                label4.Text = "Итоговая стоимость: " + sum.ToString() + "р.";
             }
-            label4.Text = "Итоговая стоимость: " + sum.ToString() + "р.";
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Food milk = new Food("молоко",100,1,"л.");
-            Food nuts = new Food("орешки",123,1,"г.");
+            comboBox1.SelectedIndex = 2;
+
+            Food milk = new Food("молоко", 100, 1, "л.");
+            Food nuts = new Food("орешки", 123, 1, "г.");
 
             FoodList.Add(milk);
             FoodList.Add(nuts);
             FoodList.Add(nuts);
 
-            int sum = 0;
+            decimal sum = 0;
             foreach (Food food in FoodList)
             {
-                checkedListBox1.Items.Add(food.Name+" "+food.Price.ToString()+"p."+food.Quantity.ToString()+food.Mesurement);
-                sum+=food.Price;
+                checkedListBox1.Items.Add(food.Name + " " + food.Price.ToString() + "p." + food.Quantity.ToString() + food.Mesurement);
+                sum += food.Price;
             }
-            label4.Text = "Итоговая стоимость: "+sum.ToString()+"р.";
+            label4.Text = "Итоговая стоимость: " + sum.ToString() + "р.";
         }
     }
 }
